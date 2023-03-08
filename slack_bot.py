487be17952ab13a73cbe0bb0977f94357229b1fc
sbansi01 <113473608+sbansi01@users.noreply.github.com>
2023-03-08 21:50:24 +0000

    i'm on a bot

--- slack_bot.py
+++ slack_bot.py
@@ -5,7 +5,7 @@ from slack.errors import SlackApiError
 slack_token = os.environ.get('SLACK_API_TOKEN')
 
 client = WebClient(token=slack_token)
-msg = "testing!"
+msg = f"I will be finished before you meet Gayle King"
 try:
     response = client.chat_postMessage(
         channel="slack-bots",
